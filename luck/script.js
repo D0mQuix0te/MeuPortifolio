6fcfe331ac4e761d81bafe50ae451f791e7890e5
Lucas Gonçalves <116138743+D0mQuix0te@users.noreply.github.com>
2025-07-13 12:48:39 -0300

    Delete luck directory

--- luck/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    var menubar = document.getElementById("sidemenu");
-    if (menubar.style.left == "0px"){
-        menubar.style.left = "-200px";
-        
-    }else{
-        menubar.style.left = "0px";
-    }  
-}
